ac0e8a03a1398b8f0f8f82a31ae39a1e321c956e
Michel Hardenberg <MHardenberg@proton.me>
2025-08-27 12:19:44 +0200

    up

--- external/dsa/include/string.h
+++ external/dsa/include/string.h
@@ -1,10 +1,11 @@
 #ifndef string_H
 #define string_H
-
 #include <assert.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define STRING_GROWTH_FAC 2
 
 typedef struct {
         size_t size;
@@ -51,14 +52,95 @@ int growString(String *string, size_t growthFactor) {
         return 0;
 }
 
-static inline int pushBackString(String *string, char c) {
+static inline char *const stringIndex(String *const string, size_t i) {
+        if (string == NULL) {
+                return NULL;
+        }
+
+        return (string->str + i);
+}
+
+#define stringPushBack(string, x) (_Generic((x),\
+        char: stringPushBack_c(string, x),\
+        const char: stringPushBack_c(string, x),\
+        char*: stringPushBack_cstr(string, x),\
+        const char*: stringPushBack_cstr(string, x),\
+        String*: stringPushBack_string(string, x),\
+        const String*: stringPushBack_string(string, x))
+
+static inline int stringPushBack_string(String *const string,
+                                        const String *const source) {
+        if (string == NULL || source == NULL) {
+                return 1;
+        }
+
+        if (string->size + source->size == string->cap) {
+                size_t sizeFac = (string->size + source->size) / string->size;
+                if (sizeFac < STRING_GROWTH_FAC
+                        ? growString(string, STRING_GROWTH_FAC)
+                        : growString(string, sizeFac)) {
+                        return 1;
+                }
+        }
+
+        for (size_t i = 0; i < source->size; ++i) {
+                char *d = string->str + i + string->size;
+                char *s = source->str + i;
+                if (d == NULL || s == NULL) {
+                        return 1;
+                }
+                *d = *s;
+        }
+
+        return 0;
+}
+
+static inline int stringPushBack_cstr(String *const string, const char *s) {
         if (string == NULL) {
                 return 1;
         }
-        string->str[string->size++] = c;
-        assert(0);
+
+        size_t len = strlen(s);
+        if (string->size + len == string->cap) {
+                if (growString(string, STRING_GROWTH_FAC)) {
+                        return 1;
+                }
+        }
+
+        for (size_t i = 0; i < len; ++i) {
+                char *d = stringIndex(string, i);
+                if (d == NULL) {
+                        return 1;
+                }
+                *d = s[i];
+        }
 
         return 0;
+}
+
+static inline int stringPushBack_c(String *const string, const char c) {
+        if (string == NULL) {
+                return 1;
+        }
+
+        if (string->size + 1 == string->cap) {
+                if (growString(string, STRING_GROWTH_FAC)) {
+                        return 1;
+                }
+        }
+
+        string->str[string->size++] = c;
+
+        return 0;
+}
+
+void destroyString(String *string) {
+        if (string && string->str) {
+                free(string->str);
+                string->str = NULL;
+                string->size = 0;
+                string->cap = 0;
+        }
 }
 
 #endif // string_H
